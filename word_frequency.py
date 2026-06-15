406ebc8724377a5bef2e9742ee164481f5475945
Leila <151710793+lizzyard@users.noreply.github.com>
2026-06-14 17:54:36 -0700

    Refactor word frequency functions and input handling
    Refactor word frequency analysis functions for improved functionality and readability.

--- word_frequency.py
+++ word_frequency.py
@@ -1,15 +1,14 @@
-
-
-word_count = {}
+import sys
 
 def create_dictionary(sublist):
-    for words in sorted(sublist):
+    word_count = {}
+    for words in sublist:
         word_count[words] = word_count.get(words, 0) + 1
 
     print_dictionary(word_count)
 
 def print_dictionary(dictionary):
-    for k,v in dictionary.items():
+    for k,v in sorted(dictionary.items()):
         print(k + ": " + str(v))
     greatest_count(dictionary)
 
@@ -17,22 +16,22 @@ def get_sentence():
     while True:
         print('Enter text: ')
         sentence = input(">")
-        if any(char.isupper() for char in sentence):
-            sentence = (sentence.lower()).split()
-        elif any(char.isdigit() for char in sentence):
+        if any(char.isdigit() for char in sentence):
             print('Please type letters only.')
+        elif any(char.isupper() for char in sentence):
+            sentence = (sentence.lower()).split()
         else:
             sentence = sentence.split()
-
-
         clean_words(sentence)
+        break
 def clean_words(list_words):
     for index, item in enumerate(list_words):
-        list_words[index] = item.strip('!.@#$%^&*()')
+        list_words[index] = item.strip(',?:;"\'')
     create_dictionary(list_words)
 
 def greatest_count(dictionary):
     print("Most common word: " + max(dictionary, key=dictionary.get))
     print("Count: " + str(max(dictionary.values())))
+    sys.exit()
 
 get_sentence()
